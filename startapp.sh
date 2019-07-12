0bb1a8599719c41464bfb89a817049f4bcdef06c
djaydev <33335484+djaydev@users.noreply.github.com>
2019-07-12 12:49:17 -0400

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export HOME=/config
-exec dbus-launch dbus-run-session -- krusader
+exec dbus-launch dbus-run-session -- krusader &
+sed -i 's/<body>/<body><style>body { background-color: dimgrey; }<\/style>\n/' /opt/novnc/index.html
